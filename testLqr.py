7d5f519100524b59abbf37581a39008237380fd8
Yu Ri <yul85yr@gmail.com>
2018-04-20 12:41:26 +0900

    PD control

--- testLqr.py
+++ testLqr.py
@@ -85,6 +85,80 @@ def apply_control_input(cart, pendulum, F, time_delta, theta_dot, g):
     # pendulum.theta += ((time_delta**2)*theta_double_dot) + (((pendulum.theta - theta_tminus2)*time_delta)/previous_time_delta)
 
 
+class Controller(object):
+    def __init__(self, skel, h):
+        self.h = h
+        self.skel = skel
+        ndofs = self.skel.ndofs
+        # self.qhat = self.skel.q
+        self.target = None
+
+        self.Kp = np.diagflat([0.0] * 6 + [400.0] * (ndofs - 6))
+        self.Kd = np.diagflat([0.0] * 6 + [40.0] * (ndofs - 6))
+        self.preoffset = 0.0
+
+    def compute(self):
+        skel = self.skel
+
+        # Algorithm:
+        # Stable Proportional-Derivative Controllers.
+        # Jie Tan, Karen Liu, Greg Turk
+        # IEEE Computer Graphics and Applications, 31(4), 2011.
+
+        invM = np.linalg.inv(skel.M + self.Kd * self.h)
+        # p = -self.Kp.dot(skel.q + skel.dq * self.h - self.qhat)
+        p = -self.Kp.dot(skel.q - self.target + skel.dq * self.h)
+        d = -self.Kd.dot(skel.dq)
+        qddot = invM.dot(-skel.c + p + d + skel.constraint_forces())
+        tau = p + d - self.Kd.dot(qddot) * self.h
+        #
+        # # Check the balance
+        # COP = skel.body('h_heel_left').to_world([0.05, 0, 0])
+        # offset = skel.C[0] - COP[0] + 0.03
+        # preoffset = self.preoffset
+        #
+        # # Adjust the target pose -- translated from bipedStand app of DART
+        #
+        # # foot = skel.dof_indices(["j_heel_left_1", "j_toe_left",
+        # #                          "j_heel_right_1", "j_toe_right"])
+        # foot = skel.dof_indices(["j_heel_left_1", "j_heel_right_1"])
+        # # if 0.0 < offset < 0.1:
+        # #     k1, k2, kd = 200.0, 100.0, 10.0
+        # #     k = np.array([-k1, -k2, -k1, -k2])
+        # #     tau[foot] += k * offset + kd * (preoffset - offset) * np.ones(4)
+        # #     self.preoffset = offset
+        # # elif -0.2 < offset < -0.05:
+        # #     k1, k2, kd = 2000.0, 100.0, 100.0
+        # #     k = np.array([-k1, -k2, -k1, -k2])
+        # #     tau[foot] += k * offset + kd * (preoffset - offset) * np.ones(4)
+        # #     self.preoffset = offset
+        #
+        # # # High-stiffness
+        # # k1, k2, kd = 200.0, 10.0, 10.0
+        # # k = np.array([-k1, -k2, -k1, -k2])
+        # # tau[foot] += k * offset + kd * (preoffset - offset) * np.ones(4)
+        # # print("offset = %s" % offset)
+        # # self.preoffset = offset
+        #
+        # # Low-stiffness
+        # k1, k2, kd = 20.0, 1.0, 10.0
+        # k = np.array([-k1, -k1])
+        # tau[foot] += k * offset + kd * (preoffset - offset) * np.ones(2)
+        # if offset > 0.03:
+        #     tau[foot] += 0.3 * np.array([-100.0, -100.0])
+        #     # print("Discrete A")
+        # if offset < -0.02:
+        #     tau[foot] += -1.0 * np.array([-100.0, -100.0])
+        #     # print("Discrete B")
+        # # print("offset = %s" % offset)
+        # self.preoffset = offset
+
+        # Make sure the first six are zero
+        tau[:6] = 0
+        return tau
+
+
+
 class MyWorld(pydart.World):
     def __init__(self, ):
         """
@@ -93,22 +167,33 @@ class MyWorld(pydart.World):
 
         self.force = None
         self.duration = 0
-        self.skeletons[0].body('ground').set_friction_coeff(0.02)
+        # self.skeletons[0].body('ground').set_friction_coeff(0.02)
         self.state = [0., 0., 0., 0.]
-        self.cart = Cart(0., 5.)
-        self.pendulum = Pendulum(1., 0., 1.)
+        self.cart = Cart(0., 1.)
+        self.pendulum = Pendulum(1.65*0.5, 0., 59.999663)
 
         # self.out_spline = self.generate_spline_trajectory()
         plist = [(0, 0), (0.2, 0), (0.5, -0.3), (1.0, -0.5), (1.5, -0.3), (1.8, 0), (2.0, 0.0)]
-        self.left_foot_traj = self.generate_spline_trajectory(plist)
+        self.left_foot_traj, self.left_der = self.generate_spline_trajectory(plist)
         plist = [(0, 0), (0.2, 0), (0.5, 0.3), (1.0, 0.5), (1.5, 0.3), (1.8, 0), (2.0, 0.0)]
-        self.right_foot_traj = self.generate_spline_trajectory(plist)
+        self.right_foot_traj, self.right_der = self.generate_spline_trajectory(plist)
 
         # print("out_spline", type(self.out_spline), type(self.out_spline[0]))
 
+        skel = self.skeletons[2]
+        # Set target pose
+        # self.target = skel.positions()
+        self.controller = Controller(skel, self.dt)
+
         self.gtime = 0
         self.update_target()
-        self.solve()
+
+        # self.controller.target = self.target
+
+        #self.solve()
+        self.controller.target = self.solve()
+        skel.set_controller(self.controller)
+        print('create controller OK')
 
     def generate_spline_trajectory(self, plist):
         ctr = np.array(plist)
@@ -126,20 +211,34 @@ class MyWorld(pydart.World):
         # tck, u = interpolate.splprep([x, y], k=3, s=0)
         # u = np.linspace(0, 1, num=50, endpoint=True)
         out = interpolate.splev(u3, tck)
+        print("out: ", out)
+        der = interpolate.splev(u3, tck, der = 1)
+        print("der: ", der)
 
         # print("x", out[0])
         # print("y", out[1])
 
-        return out
+        return out, der
 
     def update_target(self, ):
         skel1 = self.skeletons[1]
-        print("self.gtime:", self.gtime)
+        # print("self.gtime:", self.gtime)
         # self.target_foot = np.array([skel1.q["j_cart_x"], -0.92, skel1.q["j_cart_z"]])
-        self.target_left_foot = np.array([skel1.q["j_cart_x"], -0.92, self.left_foot_traj[1][self.gtime]-0.05])
-        self.target_right_foot = np.array([skel1.q["j_cart_x"], -0.92, self.right_foot_traj[1][self.gtime]+0.05])
-        l = self.skeletons[1].body('h_pole').local_com()[1]
-        self.target_pelvis = np.array([skel1.q["j_cart_x"] - l*math.sin(self.pendulum.theta)/2, l*math.cos(self.pendulum.theta)/2, 0])
+
+        # ground_height = -1.1
+        ground_height = -0.92
+        self.target_left_foot = np.array([skel1.q["j_cart_x"], ground_height, self.left_foot_traj[1][self.gtime]-0.05-0.3])
+        self.target_right_foot = np.array([skel1.q["j_cart_x"], ground_height, self.right_foot_traj[1][self.gtime]+0.05+0.3])
+        # l = self.skeletons[1].body('h_pole').local_com()[1]
+        l = self.pendulum.length
+        self.target_pelvis = np.array([skel1.q["j_cart_x"] - l*math.sin(self.pendulum.theta), l*math.cos(self.pendulum.theta), 0])
+
+        # # Update PD-control target
+        # self.target = self.skeletons[2].positions()
+        # self.target[("j_bicep_left_x", "j_bicep_right_x")] = 1.5, -1.5
+        # self.target[("j_heel_left_2")] = self.left_foot_traj[1][self.gtime] - 0.05
+        # self.target[("j_heel_right_2")] = self.right_foot_traj[1][self.gtime] + 0.05
+
         # print("target", self.target_pelvis)
 
         # print("target", self.target_pelvis)
@@ -180,9 +279,34 @@ class MyWorld(pydart.World):
         right_foot_state = self.skeletons[2].body("h_heel_right").to_world([0.0, 0.0, 0.0])
         right_foot_ori = self.skeletons[2].body("h_heel_right").world_transform()
 
-        # print("foot_ori: ", foot_ori[:3, :3])
-        left_axis_angle =rotationMatrixToEulerAngles(left_foot_ori[:3, :3])
-        right_axis_angle = rotationMatrixToEulerAngles(right_foot_ori[:3, :3])
+        # print("foot_ori: ", left_foot_ori[:3, :3], type(left_foot_ori[:3, :3]))
+
+        # todo : calculate the x_axis according to the spline
+
+        x_axis = np.array([0., 0., self.left_der[0][self.gtime]])
+        x_axis = x_axis / np.linalg.norm(x_axis)            #normalize
+        y_axis = np.array([0., 1., 0.])
+        z_axis = np.cross(x_axis, y_axis)
+        z_axis = z_axis / np.linalg.norm(z_axis)            #normalize
+        R_des = np.stack((x_axis, y_axis, z_axis), axis=-1)
+        # print("x_axis: \n")
+        # print(x_axis)
+        # print("y_axis: \n")
+        # print(y_axis)
+        # print("z_axis: \n")
+        # print(z_axis)
+        # print("R: \n")
+        # print(R_des)
+
+        # left_axis_angle =rotationMatrixToEulerAngles(left_foot_ori[:3, :3])
+        # right_axis_angle = rotationMatrixToEulerAngles(right_foot_ori[:3, :3])
+
+        left_res_rot = np.transpose(R_des).dot(left_foot_ori[:3, :3])
+        right_res_rot = np.transpose(R_des).dot(right_foot_ori[:3, :3])
+        # print("vec res: ", left_res_rot)
+        left_axis_angle =rotationMatrixToEulerAngles(left_res_rot)
+        right_axis_angle = rotationMatrixToEulerAngles(right_res_rot)
+
         # print("axis_angle", axis_angle)
 
         # return 0.5 *  np.linalg.norm(left_foot_state - self.target_foot) ** 2
@@ -217,22 +341,56 @@ class MyWorld(pydart.World):
         return g
 
     def solve(self, ):
+        q_backup = self.skeletons[2].q
         res = minimize(self.f, x0=self.skeletons[2].q, jac=self.g, method="SLSQP")
         # res = minimize(self.f, x0=self.skeletons[2].q[6:], jac=self.g, method="SLSQP")
         # print(res)
+        self.skeletons[2].set_positions(q_backup)
+
+        return res['x']
 
     def step(self):
-        F = find_lqr_control_input(self.cart, self.pendulum, -9.8)
-        apply_control_input(self.cart, self.pendulum, F, 0.01, self.state[3], -9.8)
+        # todo : Update pendulum state according to present character state
+        skel = world.skeletons[1]
+        q = skel.q
+
+        foot_center = .5 * (world.skeletons[2].body("h_heel_left").com() + world.skeletons[2].body("h_heel_right").com())
+        # self.cart.x = world.skeletons[2].body("h_heel_left").to_world([0.0, 0.0, 0.0])[0]
+        self.cart.x = foot_center[0]
+        self.cart.x_dot = world.skeletons[2].body("h_heel_left").com_linear_velocity()[0]
+        com_pos = world.skeletons[2].com()
+        # v1 = com_pos - world.skeletons[2].body("h_heel_left").to_world([0.0, 0.0, 0.0])
+        v1 = com_pos - foot_center
+        v2 = np.array([0., 1., 0.])
+        v1 = v1 / np.linalg.norm(v1)    # normalize
+        self.pendulum.theta = np.sign(-v1[0]) * math.acos(v1.dot(v2))
+        self.pendulum.theta_dot = self.pendulum.length * (world.skeletons[2].com_velocity()[0] - self.cart.x_dot) * math.cos(self.pendulum.theta)
+
         self.state[0] = self.cart.x
         self.state[1] = self.cart.x_dot
         self.state[2] = self.pendulum.theta
         self.state[3] = self.pendulum.theta_dot
-        skel = world.skeletons[1]
-        q = skel.q
+
         q["j_cart_x"] = self.state[0]
         q["j_cart_z"] = 0.
-        q["default"] = 0.
+        # q["default"] = -0.92
+        q["j_pole_x"] = 0.
+        q["j_pole_y"] = 0.
+        q["j_pole_z"] = self.state[2]
+
+        skel.set_positions(q)
+
+        F = find_lqr_control_input(self.cart, self.pendulum, -9.8)
+        apply_control_input(self.cart, self.pendulum, F, 0.01, self.state[3], -9.8)
+
+        self.state[0] = self.cart.x
+        self.state[1] = self.cart.x_dot
+        self.state[2] = self.pendulum.theta
+        self.state[3] = self.pendulum.theta_dot
+
+        q["j_cart_x"] = self.state[0]
+        q["j_cart_z"] = 0.
+        # q["default"] = 0.
         q["j_pole_x"] = 0.
         q["j_pole_y"] = 0.
         q["j_pole_z"] = self.state[2]
@@ -240,16 +398,22 @@ class MyWorld(pydart.World):
         # print("q", q)
 
         #solve ik
-        super(MyWorld, self).step()
-
         if (len(self.left_foot_traj[0]) -1) > self.gtime:
             self.gtime = self.gtime + 1
         else:
             self.gtime = self.gtime
 
-        skel.set_positions(q)
         self.update_target()
-        self.solve()
+        self.controller.target = self.solve()
+        # print(self.controller.target)
+
+        super(MyWorld, self).step()
+        skel.set_positions(q)
+
+    def on_step_event(self, ):
+         if self.force is not None and self.duration >= 0:
+             self.duration -= 1
+             self.skeletons[2].body('h_spine').add_ext_force(self.force)
 
     def on_key_press(self, key):
         if key == '1':
@@ -262,11 +426,11 @@ class MyWorld(pydart.World):
             print('push backward: f = %s' % self.force)
 
     def render_with_ri(self, ri):
-        # if self.force is not None and self.duration >= 0:
-        #     p0 = self.skeletons[1].body('h_spine').C
-        #     p1 = p0 + 0.01 * self.force
-        #     ri.set_color(1.0, 0.0, 0.0)
-        #     ri.render_arrow(p0, p1, r_base=0.05, head_width=0.1, head_len=0.1)
+        if self.force is not None and self.duration >= 0:
+            p0 = self.skeletons[2].body('h_spine').C
+            p1 = p0 + 0.01 * self.force
+            ri.set_color(1.0, 0.0, 0.0)
+            ri.render_arrow(p0, p1, r_base=0.05, head_width=0.1, head_len=0.1)
         # ri.render_sphere(self.mPendulum1.calcCOMpos(), 0.05)        #mPendulum1 COM position
 
         # ri.render_sphere(np.array([2.0, 0.0, 0.0]), 0.05)
@@ -277,8 +441,6 @@ class MyWorld(pydart.World):
         # ri.render_sphere([self.desiredTraj1[2000, 0], 0.2, 0], 0.02)
         ri.render_sphere([2.0, -0.90, 0], 0.05)
         ri.render_sphere([0.0, -0.90, 0], 0.05)
-
-
 
         for ctr_n in range(0, len(self.left_foot_traj[0])-1, 10):
             ri.set_color(0., 1., 0.)
@@ -302,75 +464,6 @@ class MyWorld(pydart.World):
         # ri.render_line(np.array([0, 0, 0]), np.array([0.0, 0, 0.3])
 
 
-class Controller(object):
-    def __init__(self, skel, h):
-        self.h = h
-        self.skel = skel
-        ndofs = self.skel.ndofs
-        self.qhat = self.skel.q
-        self.Kp = np.diagflat([0.0] * 6 + [400.0] * (ndofs - 6))
-        self.Kd = np.diagflat([0.0] * 6 + [40.0] * (ndofs - 6))
-        self.preoffset = 0.0
-
-    def compute(self):
-        skel = self.skel
-
-        # Algorithm:
-        # Stable Proportional-Derivative Controllers.
-        # Jie Tan, Karen Liu, Greg Turk
-        # IEEE Computer Graphics and Applications, 31(4), 2011.
-
-        invM = np.linalg.inv(skel.M + self.Kd * self.h)
-        p = -self.Kp.dot(skel.q + skel.dq * self.h - self.qhat)
-        d = -self.Kd.dot(skel.dq)
-        qddot = invM.dot(-skel.c + p + d + skel.constraint_forces())
-        tau = p + d - self.Kd.dot(qddot) * self.h
-
-        # Check the balance
-        COP = skel.body('h_heel_left').to_world([0.05, 0, 0])
-        offset = skel.C[0] - COP[0] + 0.03
-        preoffset = self.preoffset
-
-        # Adjust the target pose -- translated from bipedStand app of DART
-
-        # foot = skel.dof_indices(["j_heel_left_1", "j_toe_left",
-        #                          "j_heel_right_1", "j_toe_right"])
-        foot = skel.dof_indices(["j_heel_left_1", "j_heel_right_1"])
-        # if 0.0 < offset < 0.1:
-        #     k1, k2, kd = 200.0, 100.0, 10.0
-        #     k = np.array([-k1, -k2, -k1, -k2])
-        #     tau[foot] += k * offset + kd * (preoffset - offset) * np.ones(4)
-        #     self.preoffset = offset
-        # elif -0.2 < offset < -0.05:
-        #     k1, k2, kd = 2000.0, 100.0, 100.0
-        #     k = np.array([-k1, -k2, -k1, -k2])
-        #     tau[foot] += k * offset + kd * (preoffset - offset) * np.ones(4)
-        #     self.preoffset = offset
-
-        # # High-stiffness
-        # k1, k2, kd = 200.0, 10.0, 10.0
-        # k = np.array([-k1, -k2, -k1, -k2])
-        # tau[foot] += k * offset + kd * (preoffset - offset) * np.ones(4)
-        # print("offset = %s" % offset)
-        # self.preoffset = offset
-
-        # Low-stiffness
-        k1, k2, kd = 20.0, 1.0, 10.0
-        k = np.array([-k1, -k1])
-        tau[foot] += k * offset + kd * (preoffset - offset) * np.ones(2)
-        if offset > 0.03:
-            tau[foot] += 0.3 * np.array([-100.0, -100.0])
-            # print("Discrete A")
-        if offset < -0.02:
-            tau[foot] += -1.0 * np.array([-100.0, -100.0])
-            # print("Discrete B")
-        # print("offset = %s" % offset)
-        self.preoffset = offset
-
-        # Make sure the first six are zero
-        tau[:6] = 0
-        return tau
-
 if __name__ == '__main__':
     print('Example: test IPC in 3D')
 
@@ -392,19 +485,18 @@ if __name__ == '__main__':
     q["j_bicep_left_x", "j_bicep_left_y", "j_bicep_left_z"] = 1.5, 0.0, 0.0
     q["j_bicep_right_x", "j_bicep_right_y", "j_bicep_right_z"] = -1.5, 0.0, 0.0
 
-
     skel.set_positions(q)
     print('skeleton position OK')
 
-    # print('[Joint]')
-    # for joint in skel.joints:
-        # print("\t" + str(joint))
-        # print("\t\tparent = " + str(joint.parent_bodynode))
-        # print("\t\tchild = " + str(joint.child_bodynode))
-        # print("\t\tdofs = " + str(joint.dofs))
+    print('[Joint]')
+    for joint in skel.joints:
+        print("\t" + str(joint))
+        print("\t\tparent = " + str(joint.parent_bodynode))
+        print("\t\tchild = " + str(joint.child_bodynode))
+        print("\t\tdofs = " + str(joint.dofs))
 
-
-    # skel.set_controller(Controller(skel, world.dt))
-    # print('create controller OK')
+    # Set joint damping
+    # for dof in skel.dofs:
+    #     dof.set_damping_coefficient(80.0)
 
     pydart.gui.viewer.launch_pyqt5(world)
