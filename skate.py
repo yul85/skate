231be071827951c0044d005e5fcdbe9bc508d50c
Yu Ri <yul85yr@gmail.com>
2018-05-14 19:38:41 +0900

    skate.py --> simulate "pushing side to side" skating motion using SIMBICON

---
+++ skate.py
@@ -0,0 +1,170 @@
+import numpy as np
+import pydart2 as pydart
+import QPsolver
+
+class State(object):
+    def __init__(self, dt, c_d, c_v, angles):
+        self.dt = dt
+        self.c_d = c_d
+        self.c_v = c_v
+        self.angles = angles
+
+class MyWorld(pydart.World):
+    def __init__(self, ):
+        pydart.World.__init__(self, 1.0 / 1000.0, './data/skel/cart_pole_blade.skel')
+        # pydart.World.__init__(self, 1.0 / 2000.0, './data/skel/cart_pole.skel')
+        self.force = None
+        self.duration = 0
+        self.skeletons[0].body('ground').set_friction_coeff(0.02)
+
+        skel = self.skeletons[2]
+        backup_q = skel.q
+        q = skel.q
+
+        q["j_abdomen_2"] = -0.2
+        # q["j_thigh_right_x", "j_thigh_right_y"] = -0.2, -0.7
+        # q["j_thigh_left_x", "j_thigh_left_y"] = 0.2, 0.7
+        q["j_thigh_right_x", "j_thigh_right_y", "j_thigh_right_z"] = -0.2, -0.7, 0.5
+        q["j_thigh_left_x", "j_thigh_left_y", "j_thigh_left_z"] = 0.5, 0.7, -0.5
+        q["j_shin_left"] = -0.0
+        q["j_shin_right"] = -0.3
+        # q["j_heel_left_2"] = 0.7
+        # q["j_heel_right_2"] = -0.7
+        q["j_bicep_left_x", "j_bicep_left_y", "j_bicep_left_z"] = 1.5, 0.0, 0.0
+        q["j_bicep_right_x", "j_bicep_right_y", "j_bicep_right_z"] = -1.5, 0.0, 0.0
+
+        # skel.set_positions(q)
+        self.skeletons[3].set_positions(q)
+
+        state0 = State(0.5, 0.0, 0.2, q)
+
+        q["j_abdomen_2"] = -0.2
+        q["j_thigh_left_z", "j_shin_left", "j_heel_left_1"] = 0.30, -0.5, 0.25
+        q["j_thigh_right_z", "j_shin_right", "j_heel_right_1"] = 0.30, -0.5, 0.25
+        q["j_thigh_right_x", "j_thigh_right_z"] = -0.5, -0.5
+
+        q["j_heel_left_2"] = 0.7
+        q["j_heel_right_2"] = -0.7
+        q["j_bicep_left_x", "j_bicep_left_y", "j_bicep_left_z"] = 1.5, 0.0, 0.0
+        q["j_bicep_right_x", "j_bicep_right_y", "j_bicep_right_z"] = -1.5, 0.0, 0.0
+
+        # skel.set_positions(q)
+
+        state1 = State(0.5, 2.2, 0.0, q)
+        self.state_list = [state0, state1]
+
+        skel.set_positions(backup_q)
+
+        self.curr_state = self.state_list[0]
+        self.elapsedTime = 0.0
+        self.curr_state_index = 0
+        # print("backup angle: ", backup_q)
+        print("cur angle: ", self.curr_state.angles)
+        self.controller = QPsolver.Controller(skel, self.dt)
+
+        self.controller.target = self.curr_state.angles
+        # self.controller.target = skel.q
+        skel.set_controller(self.controller)
+        print('create controller OK')
+
+        self.contact_force = []
+
+    def step(self):
+
+        if self.curr_state.dt < self.time() - self.elapsedTime:
+            print("change the state!!!", self.curr_state_index)
+            self.curr_state_index = self.curr_state_index + 1
+            self.curr_state_index = self.curr_state_index % 2
+            self.elapsedTime = self.time()
+            self.curr_state = self.state_list[self.curr_state_index]
+            print("state_", self.curr_state_index)
+
+        # self.controller.target = skel.q
+        self.controller.target = self.curr_state.angles
+
+        del self.contact_force[:]
+        if len(self.controller.sol_lambda) != 0:
+            f_vec = self.controller.V_c.dot(self.controller.sol_lambda)
+            # print("f", f_vec)
+            f_vec = np.asarray(f_vec)
+            # print("contact num ?? : ", self.controller.contact_num)
+            # self.contact_force = np.zeros(self.controller.contact_num)
+            for ii in range(self.controller.contact_num):
+                self.contact_force.append(np.array([f_vec[3*ii], f_vec[3*ii+1], f_vec[3*ii+2]]))
+                # self.contact_force[ii] = np.array([f_vec[3*ii], f_vec[3*ii+1], f_vec[3*ii+2]])
+                # print("contact_force:", ii, self.contact_force[ii])
+
+            # print("contact_force:\n", self.contact_force)
+            for ii in range(self.controller.contact_num):
+                self.skeletons[2].body(self.controller.contact_list[2 * ii])\
+                    .add_ext_force(self.contact_force[ii], self.controller.contact_list[2 * ii+1])
+
+        super(MyWorld, self).step()
+
+        # skel.set_positions(q)
+
+    def on_key_press(self, key):
+        if key == '1':
+            self.force = np.array([500.0, 0.0, 0.0])
+            self.duration = 1000
+            print('push backward: f = %s' % self.force)
+        elif key == '2':
+            self.force = np.array([-50.0, 0.0, 0.0])
+            self.duration = 100
+            print('push backward: f = %s' % self.force)
+
+    def render_with_ri(self, ri):
+        if self.force is not None and self.duration >= 0:
+            p0 = self.skeletons[2].body('h_pelvis').C
+            p1 = p0 + 0.01 * self.force
+            ri.set_color(1.0, 0.0, 0.0)
+            ri.render_arrow(p0, p1, r_base=0.05, head_width=0.1, head_len=0.1)
+
+        # render contact force --yul
+        contact_force = self.contact_force
+
+        if len(contact_force) != 0:
+            # print(len(contact_force), len(self.controller.contact_list))
+            # print("contact_force.size?", contact_force.size, len(contact_force))
+            ri.set_color(1.0, 0.0, 0.0)
+            for ii in range(len(contact_force)):
+                if 2 * len(contact_force) == len(self.controller.contact_list):
+                    body = self.skeletons[2].body(self.controller.contact_list[2*ii])
+                    contact_offset = self.controller.contact_list[2*ii+1]
+                    # print("contact force : ", contact_force[ii])
+                    ri.render_line(body.to_world(contact_offset), contact_force[ii]/100.)
+
+        # todo : ground rendering
+        # ri.render_chessboard(5)
+        # render axes
+        ri.render_axes(np.array([0, 0, 0]), 0.5)
+
+if __name__ == '__main__':
+    print('Example: Skating -- pushing side to side')
+
+    pydart.init()
+    print('pydart initialization OK')
+
+
+    world = MyWorld()
+    print('MyWorld  OK')
+
+    skel = world.skeletons[2]
+    q = skel.q
+
+    # q["j_abdomen_1"] = -0.2
+    q["j_abdomen_2"] = -0.2
+
+    q["j_thigh_right_x", "j_thigh_right_y"] = -0.2, -0.7
+    q["j_thigh_left_x", "j_thigh_left_y"] = 0.5, 0.7
+    q["j_thigh_left_z", "j_shin_left"] = 0.30, -0.5
+    q["j_thigh_right_z", "j_shin_right"] = 0.30, -0.5
+
+    # both arm T-pose
+    q["j_bicep_left_x", "j_bicep_left_y", "j_bicep_left_z"] = 1.5, 0.0, 0.0
+    q["j_bicep_right_x", "j_bicep_right_y", "j_bicep_right_z"] = -1.5, 0.0, 0.0
+
+    skel.set_positions(q)
+    print('skeleton position OK')
+
+    pydart.gui.viewer.launch_pyqt5(world)
