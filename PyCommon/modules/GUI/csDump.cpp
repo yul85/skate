142fec946473bdc300b3d3bc889e82df0ca7813e
yuri <yul85yr@gmail.com>
2019-08-22 15:27:11 +0900

    dynamic allocation for png dumping

--- PyCommon/modules/GUI/csDump.cpp
+++ PyCommon/modules/GUI/csDump.cpp
@@ -27,10 +27,10 @@ BOOST_PYTHON_MODULE(csDump)
 
 void dump_png(char* filename, int w, int h)
 {
-
     int image_size = 3*w*h;
-    unsigned char image_tmp[image_size];
-    unsigned char image[image_size];
+    unsigned char *image_tmp = new unsigned char[image_size];
+    unsigned char *image = new unsigned char[image_size];
+
     glPixelStorei(GL_PACK_ALIGNMENT, 1);
     glReadBuffer(GL_BACK_LEFT);
     glReadPixels(0, 0, w, h, GL_RGB, GL_UNSIGNED_BYTE, image_tmp);
@@ -39,5 +39,8 @@ void dump_png(char* filename, int w, int h)
         memcpy(&image[3*w*(h-1-i)], &image_tmp[3*w*i], 3*w*sizeof(unsigned char));
 
     unsigned int error = lodepng_encode24_file(filename, image, w, h);
-    if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
+
+    if (error) printf("error %u: %s\n", error, lodepng_error_text(error));
+    if (image_tmp) delete [] image_tmp;
+    if (image) delete [] image;
 }
